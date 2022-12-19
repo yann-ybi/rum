9559cdfed2be3dc9fb611dcfbae1dd46b003f627
Yann Youbi <youbi.yann@gmail.com>
2022-12-19 00:39:37 -0500

    back to vec of vec for sandmark

--- src/machine/memory/memory.rs
+++ src/machine/memory/memory.rs
@@ -1,45 +1,40 @@
-use std::{usize, vec};
+use std::{collections::VecDeque, usize, vec};
 
 /// Memory structure of the UM machine
 pub struct Memory {
-    seg0: Box<[u32]>,
     // segmented memory
     pub segs: Vec<Vec<u32>>,
     // unmapped segments ready to be mapped again
-    pub unmapped_segs: Vec<usize>
+    pub unmapped_segs: VecDeque<usize>
 }
 impl Memory {
     /// Memory constructor function
     pub fn new(instructions: Vec<u32>) -> Self {
 
         Memory {
-            seg0: instructions.into_boxed_slice(),
-            segs: Vec::new(),
-            unmapped_segs: Vec::new()
+            
+            segs: vec![instructions],
+            unmapped_segs: VecDeque::new()
         }
     }
     /// return a word from the segmented memory based on its id and offset
     /// return None if no word found at that location
     pub fn get(&mut self, id: u32, offset: u32) -> u32 {
-        if id == 0 {
-            self.seg0[offset as usize]
-        }
-        else{ self.segs[id as usize][offset as usize] }
+
+        self.segs[id as usize][offset as usize]
 
     }
     /// set a word in the segmented memory based on its id and offset
     /// return None if no word found at that location
     pub fn set(&mut self, id: u32, offset: u32, val: u32) {
-        if id == 0 {
-            self.seg0[offset as usize] = val
-        }
-        else {self.segs[id as usize][offset as usize] = val}
+        
+        self.segs[id as usize][offset as usize] = val
 
     }
     pub fn allocate(&mut self, len: usize) -> usize {
         let new_seg: Vec<u32> = vec![0_u32; len];
 
-        match self.unmapped_segs.pop() {
+        match self.unmapped_segs.pop_front() {
             Some(o) => {
                 self.segs[o] = new_seg;
                 o
@@ -52,7 +47,7 @@ impl Memory {
     }
     pub fn deallocate(&mut self, id: usize) {
         self.segs[id] = Vec::new();
-        self.unmapped_segs.push(id)
+        self.unmapped_segs.push_back(id)
     }
 
 
